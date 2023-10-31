387e80edfa90f765334e85741badf4024bd39d5e
sanyam <patilsanyam869@gmail.com>
2023-10-31 18:46:45 +0530

    features are created but not working

---
+++ js-2/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("mousemove", function (details) {
+    var rect = document.querySelector("#rect")
+
+    var xval = gsap.utils.mapRange(
+        0,
+        window.innerWidth,
+        100 + rect.getBoundingClientRect().widht / 2,
+
+        window.innerWidth - (100 + rect.getBoundingClientRect().widht / 2),
+        details.clientX
+    );
+
+    gsap.to("#rect", {
+        left: xval,
+        ease: Power3,
+    })
+})
